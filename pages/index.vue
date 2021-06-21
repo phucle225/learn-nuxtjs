8410d35fd1034e36a31e2cb818e00ab9d0f59cb5
vinhvo <phucle225@gmail.com>
2021-06-21 17:09:57 +0700

    up

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div v-for="item in data" :key="item.id">
+      <div>{{ item.username }} {{ item.password }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, $strapi }) {
+    const data = await $strapi.$tests.find()
+    return { data }
+  },
+}
+</script>
